11b41605669003a4c6ef8b3cfdb5e8b63537542b
Vedat <vedat.botuk@gmail.com>
2024-11-15 09:15:06 +0100

    Refactor battery measurement functions for improved error handling and clarity; update CMakeLists.txt to specify include directory

--- components/update_cluster/include/update_cluster.h
+++ components/update_cluster/include/update_cluster.h
@@ -19,11 +19,8 @@
 #define ZB_TEMP_SENSOR_H
 
 void zb_update_temp(int temperature);
-void zb_report_temp();
 void zb_update_hum(int humidity);
-void zb_report_hum();
 void zb_update_battery_level(int level, int voltage);
-void zb_report_battery_level();
 void zb_update_waterleak(uint16_t leak);
 void zb_report_waterleak(uint16_t leak);
 
