b545764c49450f70ccf3b9037798bc805fcd6ec5
Vedat <vedat.botuk@gmail.com>
2024-11-21 10:57:06 +0100

    Add new Zigbee device definitions and update existing ones for ESP32H2 sensors

--- homeassistant-zigbee2mqtt/ESP32H2_Sensor_50176.js
+++ homeassistant-zigbee2mqtt/ESP32H2_Sensor_50176.js
@@ -1,11 +1,12 @@
-// This external converter should be tested.
 const ota = require('zigbee-herdsman-converters/lib/ota');
 
 const definition = {
   zigbeeModel: ['50176'], // The model ID from: Device with modelID 'lumi.sens' is not supported.
-  model: 'ESP32H2_Sensor', // Vendor model number, look on the device for a model number
+  model: '50176', // Vendor model number, look on the device for a model number
   vendor: 'Botuk', // Vendor of the device (only used for documentation and startup logging)
-  description: 'test device', // Description of the device, copy from vendor site. (only used for documentation and startup logging)
+  description: 'test device light-sleep', // Description of the device, copy from vendor site. (only used for documentation and startup logging)
+  extend: [],
+  meta: {},
   configure: async (device, coordinatorEndpoint, logger) => {
     const endpoint = device.getEndpoint(10);
   },
