c22037e0f2ed75aeda43c10182e37dfe7169f44c
Vedat <vedat.botuk@gmail.com>
2024-12-03 23:55:11 +0100

    fix unused function

--- main/device.c
+++ main/device.c
@@ -300,9 +300,7 @@ static void esp_zb_task(void *pvParameters)
     esp_zb_ep_list_add_ep(esp_zb_ep_list, esp_zb_cluster_list, endpoint_config);
 
     esp_zb_device_register(esp_zb_ep_list);
-#if defined OTA_UPDATE || defined SWITCH
     esp_zb_core_action_handler_register(zb_action_handler);
-#endif
     esp_zb_set_primary_network_channel_set(ESP_ZB_PRIMARY_CHANNEL_MASK);
     ESP_ERROR_CHECK(esp_zb_start(false));
     esp_zb_stack_main_loop();
